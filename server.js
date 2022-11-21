3698556c77f5a6a8897fe8ac4a41843ff96310b9
Seonghun <77530278+kawai10@users.noreply.github.com>
2022-11-21 15:58:37 +0900

    Mutation Type
    단순히 데이터를 가져오는 GET의 경우에는 Query Type에 작성하지만 POST,PUT,DELETE 경우에는 Mutation type에 작성해야함

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ const typeDefs = gql`
         allTweets : [Tweet]
         tweet(id :ID) : Tweet
     }
+    
+    type Mutation {
+        postTweet(text:String, userId:ID) : Tweet
+        deleteTweet(id:ID):Boolean
+    }
 `
 
 const server = new ApolloServer({typeDefs})
