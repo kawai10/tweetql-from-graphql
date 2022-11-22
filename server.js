038bb5211224f5527e0049be7a9fee0c50ad8d01
Seonghun <77530278+kawai10@users.noreply.github.com>
2022-11-22 14:56:27 +0900

    Migrating from REST to GraphQL

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import {ApolloServer, gql} from "apollo-server";
+import fetch from 'node-fetch'
 
 let tweets = [
     {
@@ -45,15 +46,41 @@ const typeDefs = gql`
         author : User
     }
     type Query {
+        allMovies: [Movie!]!
         allTweets : [Tweet!]!
         allUsers : [User!]!
         tweet(id :ID!) : Tweet
         ping : String
+        movie(id: String): Movie!
     }
     
     type Mutation {
         postTweet(text:String, userId:ID) : Tweet
         deleteTweet(id:ID):Boolean
+    }
+
+    type Movie {
+        id: Int!
+        url: String!
+        imdb_code: String!
+        title: String!
+        title_english: String!
+        title_long: String!
+        slug: String!
+        year: Int!
+        rating: Float!
+        runtime: Float!
+        genres: [String]!
+        summary: String
+        description_full: String!
+        synopsis: String
+        yt_trailer_code: String!
+        language: String!
+        background_image: String!
+        background_image_original: String!
+        small_cover_image: String!
+        medium_cover_image: String!
+        large_cover_image: String!
     }
 `
 
@@ -69,6 +96,12 @@ const resolvers = {
         }
     },
     Query : {
+        allMovies() {
+          return fetch('https://yts.mx/api/v2/list_movies.json').then(res => res.json()).then(json=> json.data.movies)
+        },
+        movie(_,{id}){
+            return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`).then(res => res.json()).then(json=> json.data.movie)
+        },
         allTweets() {
             return tweets
         },
